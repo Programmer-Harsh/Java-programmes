1772eaef413e65b4be48c6c0a513c3714565e45e
Programmer-Harsh <146876554+Programmer-Harsh@users.noreply.github.com>
2023-10-06 21:54:49 +0530

    Update Concatenate.java
    fixes some error

--- Concatenate.java
+++ Concatenate.java
@@ -1,10 +1,9 @@
 //import java.io.Console;
 public class Concatenate{
     public static void main(String args[]){
-        //Console c = System.console();
-        //String s1 = c.readLine("Enter the number : ");
-        //String s2 = c.readLine("Enter the other number : ");
-        //System.out.println(s1 + s2);
-        System.out.println("1"+2+3);
+        Console c = System.console();
+        String s1 = c.readLine("Enter the number : ");
+        String s2 = c.readLine("Enter the other number : ");
+        System.out.println(s1 + s2);
     }
 }
